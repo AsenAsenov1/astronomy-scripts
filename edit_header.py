19628286ac9825e2ef70a126c69968fea4d25975
Asen Asenov <asen991@gmail.com>
2023-08-13 10:44:14 +0300

    Update edit_header.py

--- edit_header.py
+++ edit_header.py
@@ -1,27 +1,26 @@
-import os
 from astropy.io import fits
-
+import os
+import sys
 
 # Fill the values
 header_data = {
-    "OBJECT": "",
-    "RA": "",
-    "DEC": "",
+    "OBJECT": "  ",
+    "RA": "  ",
+    "DEC": "  ",
     "INSTRUME": "FLI",
     "TELESCOP": "SCHMIDT"
 }
 
-# Get the current directory
-current_dir = os.getcwd()
-file_names = os.listdir(current_dir)
+dir_path = sys.argv[1]  # Target directory in which .fit files are located
+file_names = os.listdir(dir_path)
 
-# Insert file names into a list as strings
-file_names_list = [(str(file_name)) for file_name in file_names if ".fit" in file_name]
-
-# Iterate through every file and write the following values
-for file in file_names_list:
-    fits.setval(file, "OBJECT", value=header_data["OBJECT"])
-    fits.setval(file, "RA", value=header_data["RA"])
-    fits.setval(file, "DEC", value=header_data["DEC"])
-    fits.setval(file, "INSTRUME", value=header_data["INSTRUME"])
-    fits.setval(file, "TELESCOP", value=header_data["TELESCOP"])
+for file in file_names:
+    file_path = os.path.join(dir_path, file)
+    if os.path.isfile(file_path):
+        if ".fit" in str(file):
+            fits.setval(file_path, "OBJECT", value=header_data["OBJECT"])
+            fits.setval(file_path, "RA", value=header_data["RA"])
+            fits.setval(file_path, "DEC", value=header_data["DEC"])
+            fits.setval(file_path, "INSTRUME", value=header_data["INSTRUME"])
+            fits.setval(file_path, "TELESCOP", value=header_data["TELESCOP"])
+            print(f"File {file} header is now updated.")
